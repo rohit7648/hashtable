e991bcac7124ce1983e499528cb2214270fb732b
rohit7648 <46596305+rohit7648@users.noreply.github.com>
2020-10-04 14:46:11 +0530

    Add files via upload

---
+++ group_anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>m;
+         vector<vector<string>>result;
+        for(string s:strs){
+            string temp=s;
+            sort(s.begin(),s.end());
+            m[s].push_back(temp);
+        }
+        unordered_map<string,vector<string>>::iterator it;
+        for(it=m.begin();it!=m.end();it++){
+            result.push_back(it->second);
+        }
+       
+        return result;
+    }
+};
